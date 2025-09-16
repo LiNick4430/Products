b8a3c1eef2e1389aa2ab889d6078c1f01f4cac15
User <User@DESKTOP-G198TRC>
2025-09-16 13:24:30 +0800

    20250916

--- daycare-system-springboot/src/main/java/com/github/lianick/service/UserService.java
+++ daycare-system-springboot/src/main/java/com/github/lianick/service/UserService.java
@@ -1,5 +1,13 @@
 package com.github.lianick.service;
 
+import java.util.List;
+
+import com.github.lianick.model.User;
+
 public interface UserService {
 
+	List<User> findAllUser();
+	User getUserById(Long id);
+	User getUserByAccount(String account);
+	
 }
