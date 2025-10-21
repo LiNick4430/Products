c5cb66345bf06eed42630cea8b0525486c552e2c
User <User@host.docker.internal>
2025-10-21 14:20:15 +0800

    pc/user

--- daycare-system-springboot/src/main/java/com/github/lianick/controller/AuthController.java
+++ daycare-system-springboot/src/main/java/com/github/lianick/controller/AuthController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.github.lianick.model.dto.user.UserLoginDTO;
 import com.github.lianick.model.dto.user.UserUpdateDTO;
+import com.github.lianick.model.dto.userPublic.UserPublicUpdateDTO;
 import com.github.lianick.response.ApiResponse;
+import com.github.lianick.service.UserPublicService;
 import com.github.lianick.service.UserService;
 
 import jakarta.servlet.http.HttpSession;
@@ -19,9 +21,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 /**
  * AuthContorller
  * Request Mapping: "/auth"
- * POST	"/login/"			登入			"/auth/login/"
- * GET  "/logout/"			登出			"/auth/logout/"
- * POST	"/check/password/"	修改前確認密碼	"/auth/check/password/"
+ * POST	"/login/"					登入				"/auth/login/"
+ * GET  "/logout/"					登出				"/auth/logout/"
+ * POST	"/check/password/"			修改前      確認密碼	"/auth/check/password/"
+ * POST	"/public/check/password/"	民眾資料修改前 確認密碼	"/auth/public/check/password/"
  * */
 
 @RestController
@@ -32,23 +35,31 @@ public class AuthController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private UserPublicService userPublicService;
+	
 	@PostMapping("/login/")
 	public ApiResponse<UserLoginDTO> login (@RequestBody UserLoginDTO userLoginDTO, HttpSession httpSession) {
 		userLoginDTO = userService.loginUser(userLoginDTO);
 		httpSession.setAttribute("userLoginDTO", userLoginDTO);
-		return new ApiResponse<UserLoginDTO>(HttpStatus.OK.value(), "登陸成功", userLoginDTO);
+		return new ApiResponse<>(HttpStatus.OK.value(), "登陸成功", userLoginDTO);
 	}
 	
 	@GetMapping("/logout/")
 	public ApiResponse<Void> logout (HttpSession httpSession) {
 		httpSession.invalidate();
-		return new ApiResponse<Void>(HttpStatus.OK.value(), "登出成功", null);
+		return new ApiResponse<>(HttpStatus.OK.value(), "登出成功", null);
 	}
 	
 	@PostMapping("/check/password/")
 	public ApiResponse<UserUpdateDTO> updateCheckPassword (@RequestBody UserUpdateDTO userUpdateDTO) {
 		userUpdateDTO = userService.updateUserCheckPassword(userUpdateDTO);
-		return new ApiResponse<UserUpdateDTO>(HttpStatus.OK.value(), "密碼確認成功, 進入修改資料網頁", userUpdateDTO);
+		return new ApiResponse<>(HttpStatus.OK.value(), "密碼確認成功, 進入修改資料網頁", userUpdateDTO);
 	}
 	
+	@PostMapping("/public/check/password/")
+	public ApiResponse<UserPublicUpdateDTO> updatePublicCheckPassword (@RequestBody UserPublicUpdateDTO userPublicUpdateDTO) {
+		userPublicUpdateDTO = userPublicService.updateUserPublicCheckPassword(userPublicUpdateDTO);
+		return new ApiResponse<>(HttpStatus.OK.value(), "密碼確認成功, 進入修改資料網頁", userPublicUpdateDTO);
+	}
 }
---
+++ daycare-system-springboot/src/main/java/com/github/lianick/controller/CitizenProfileController.java
@@ -0,0 +1,70 @@
+package com.github.lianick.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.lianick.model.dto.user.UserDeleteDTO;
+import com.github.lianick.model.dto.userPublic.UserPublicCreateDTO;
+import com.github.lianick.model.dto.userPublic.UserPublicDTO;
+import com.github.lianick.model.dto.userPublic.UserPublicUpdateDTO;
+import com.github.lianick.response.ApiResponse;
+import com.github.lianick.service.UserPublicService;
+
+/**
+ * CitizenProfileController
+ * Request Mapping: "/public"
+ * GET		"/find/all/"		尋找	全部民眾帳號	"/public/find/all/"
+ * POST		"/find/"			尋找	特定民眾帳號	"/public/find/"
+ * POST		"/information/"		設定 民眾基本資料		"/public/set/information/"
+ * POST		"/update/"			更新 民眾基本資料		"/public/update/"
+ * DELETE	"/delete/"			刪除	帳號			"/public/delete/"
+ * 
+ * */
+
+@RestController
+@RequestMapping("/public")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+public class CitizenProfileController {
+	
+	@Autowired
+	private UserPublicService userPublicService;
+	
+	@GetMapping("/find/all/")
+	public ApiResponse<List<UserPublicDTO>> findAll() {
+		List<UserPublicDTO> userPublicDTOs = userPublicService.findAllUserPublic();
+		return new ApiResponse<>(HttpStatus.OK.value(), "搜尋 全部民眾資料成功", userPublicDTOs);
+	}
+	
+	@PostMapping("/find/")
+	public ApiResponse<UserPublicDTO> find(@RequestBody UserPublicDTO userPublicDTO) {
+		userPublicDTO = userPublicService.findByUsername(userPublicDTO);
+		return new ApiResponse<>(HttpStatus.OK.value(), "搜尋 民眾資料成功", userPublicDTO);
+	}
+	
+	@PostMapping("/information/")
+	public ApiResponse<UserPublicCreateDTO> information(@RequestBody UserPublicCreateDTO userPublicCreateDTO) {
+		userPublicCreateDTO = userPublicService.createUserPublic(userPublicCreateDTO);
+		return new ApiResponse<>(HttpStatus.OK.value(), "民眾資料 建立成功", userPublicCreateDTO);
+	}
+
+	@PostMapping("/update/")
+	public ApiResponse<UserPublicUpdateDTO> update(@RequestBody UserPublicUpdateDTO userPublicUpdateDTO) {
+		userPublicUpdateDTO = userPublicService.updateUserPublic(userPublicUpdateDTO);
+		return new ApiResponse<>(HttpStatus.OK.value(), "民眾資料 更新成功", userPublicUpdateDTO);
+	}
+	
+	@DeleteMapping("/delete/")
+	public ApiResponse<Void> delete(@RequestBody UserDeleteDTO userDeleteDTO) {
+		userPublicService.deleteUserPublic(userDeleteDTO);
+		return new ApiResponse<Void>(HttpStatus.OK.value(), "民眾帳號 刪除成功", null);
+	}
+}
