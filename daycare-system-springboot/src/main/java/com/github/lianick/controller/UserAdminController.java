4f0c115e25eeb4a50c2f3981aeee65111cd048db
User <User@DESKTOP-G198TRC.mshome.net>
2025-11-04 16:17:29 +0800

    pc / java

--- daycare-system-springboot/src/main/java/com/github/lianick/controller/UserAdminController.java
+++ daycare-system-springboot/src/main/java/com/github/lianick/controller/UserAdminController.java
@@ -58,13 +58,13 @@ public class UserAdminController {
 	@PostMapping(value = {"/create", "/create/"})
 	public ApiResponse<UserAdminDTO> createUserAdmin(@RequestBody UserAdminCreateDTO userAdminCreateDTO) {
 		UserAdminDTO userAdminDTO = userAdminService.createUserAdmin(userAdminCreateDTO);
-		return ApiResponse.success("設定 員工基本資料 成功", userAdminDTO);
+		return ApiResponse.success("設定 員工基本資料 成功, 請通知員工 進入信箱 啟動帳號", userAdminDTO);
 	}
 	
 	@PostMapping(value = {"/update", "/update/"})
 	public ApiResponse<UserAdminDTO> updateUserAdmin(@RequestBody UserAdminUpdateDTO userAdminUpdateDTO) {
 		UserAdminDTO userAdminDTO = userAdminService.updateUserAdmin(userAdminUpdateDTO);
-		return ApiResponse.success("設定 員工基本資料 成功", userAdminDTO);
+		return ApiResponse.success("更新 員工基本資料 成功", userAdminDTO);
 	}
 	
 	@DeleteMapping(value = {"/delete", "/delete/"})
