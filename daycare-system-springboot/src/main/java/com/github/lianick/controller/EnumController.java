c70400a7fbe2b415c13bf50ab117ed9fbd64ee17
User <User@DESKTOP-G198TRC>
2025-11-26 00:27:56 +0800

    pc/ enum

--- daycare-system-springboot/src/main/java/com/github/lianick/controller/EnumController.java
+++ daycare-system-springboot/src/main/java/com/github/lianick/controller/EnumController.java
@@ -3,8 +3,18 @@ package com.github.lianick.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 給前端工程師 用的 ENUM 街口
+ * 查詢 該 ENUM 的 code + description
+ * EnumController
+ * Request Mapping: "/enum"
+ * GET 	"/document/type" 	DocumentType
+ * */
+
 @RestController
 @RequestMapping("/enum")
 public class EnumController {
 
+	
+	
 }
